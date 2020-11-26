64fffed4ab03a9b8e4657e71d2a597b612737a48
Ray Ji <rayji96@gmail.com>
2020-11-26 04:05:01 -0800

    vampireRoll accounts for hunger, disciplines tab functional

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -9,9 +9,9 @@ export class BoilerplateActorSheet extends ActorSheet {
     return mergeObject(super.defaultOptions, {
       classes: ["vtm5e", "sheet", "actor"],
       template: "systems/vtm5e/templates/actor/actor-sheet.html",
-      width: 600,
+      width: 800,
       height: 600,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "stats" }]
     });
   }
 
@@ -21,9 +21,9 @@ export class BoilerplateActorSheet extends ActorSheet {
   getData() {
     const data = super.getData();
     data.dtypes = ["String", "Number", "Boolean"];
-    for (let attr of Object.values(data.data.attributes)) {
-      attr.isCheckbox = attr.dtype === "Boolean";
-    }
+    // for (let attr of Object.values(data.data.attributes)) {
+    //   attr.isCheckbox = attr.dtype === "Boolean";
+    // }
 
     // Prepare items.
     if (this.actor.data.type == 'character') {
@@ -44,26 +44,32 @@ export class BoilerplateActorSheet extends ActorSheet {
     const actorData = sheetData.actor;
 
     // Initialize containers.
+    const specialties = [];
     const gear = [];
     const features = [];
-    const spells = {
-      0: [],
-      1: [],
-      2: [],
-      3: [],
-      4: [],
-      5: [],
-      6: [],
-      7: [],
-      8: [],
-      9: []
+    const disciplines = {
+      "animalism": [],
+      "auspex": [],
+      "celerity": [],
+      "dominate": [],
+      "fortitude": [],
+      "obfuscate": [],
+      "potence": [],
+      "presence": [],
+      "protean": [],
+      "sorcery": [],
+      "rituals": [],
+      "alchemy": []
     };
 
     // Iterate through items, allocating to containers
-    // let totalWeight = 0;
     for (let i of sheetData.items) {
       let item = i.data;
       i.img = i.img || DEFAULT_TOKEN;
+      // Append to specialties.
+      if (i.type === 'specialty') {
+        specialties.push(i);
+      }
       // Append to gear.
       if (i.type === 'item') {
         gear.push(i);
@@ -72,18 +78,21 @@ export class BoilerplateActorSheet extends ActorSheet {
       else if (i.type === 'feature') {
         features.push(i);
       }
-      // Append to spells.
-      else if (i.type === 'spell') {
-        if (i.data.spellLevel != undefined) {
-          spells[i.data.spellLevel].push(i);
+      // Append to disciplines.
+      else if (i.type === 'power') {
+        if (i.data.discipline != undefined) {
+          disciplines[i.data.discipline].push(i);
         }
       }
     }
 
     // Assign and return
+    actorData.specialties = specialties;
     actorData.gear = gear;
     actorData.features = features;
-    actorData.spells = spells;
+    actorData.disciplines_list = disciplines;
+    console.log(actorData)
+    console.log(sheetData.items)
   }
 
   /* -------------------------------------------- */
@@ -115,6 +124,9 @@ export class BoilerplateActorSheet extends ActorSheet {
     // Rollable abilities.
     html.find('.rollable').click(this._onRoll.bind(this));
 
+    // Rollable Vampire abilities.
+    html.find('.vrollable').click(this._onVampireRoll.bind(this));
+
     // Drag events for macros.
     if (this.actor.owner) {
       let handler = ev => this._onDragItemStart(ev);
@@ -138,6 +150,9 @@ export class BoilerplateActorSheet extends ActorSheet {
     const type = header.dataset.type;
     // Grab any data associated with this control.
     const data = duplicate(header.dataset);
+    if (type == "specialty"){
+      data.skill = "" 
+    }
     // Initialize a default name.
     const name = `New ${type.capitalize()}`;
     // Prepare the item object.
@@ -173,4 +188,38 @@ export class BoilerplateActorSheet extends ActorSheet {
     }
   }
 
+    /**
+   * Handle clickable Vampire rolls.
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onVampireRoll(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const dataset = element.dataset;
+    if (dataset.roll) {
+      let roll = new Roll(dataset.roll + "d10cs>5", this.actor.data.data);
+      let label = dataset.label ? dataset.label : '';
+      let roll_result = roll.evaluate();
+      let hunger_dice = this.actor.data.data.hunger.value;
+      let success = 0;
+      let hunger_success = 0;
+      roll_result.terms[0].results.forEach((dice, index) => {
+        if (dice.success){
+          if ((index + 1) > hunger_dice){
+            success++
+          }else{
+            hunger_success++
+          }
+        }
+      });
+
+      label = label + `\nSuccesses: ${success}\nHunger Successes: ${hunger_success}\n`;
+
+      roll_result.toMessage({
+        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+        flavor: label
+      });
+    }
+  }
 }
