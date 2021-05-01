edca937045150533523c58729861c964f33f6da4
pedroboh <83445071+pedroboh@users.noreply.github.com>
2021-04-30 23:35:38 -0300

    Update actor-sheet.js

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -18,8 +18,8 @@ export class VampireActorSheet extends ActorSheet {
 
   /** @override */
   get template() {
-    if ( !game.user.isGM && !this.actor.owner ) return "systems/vtm5e/templates/actor/limited-sheet.html";
-    return "systems/vtm5e/templates/actor/actor-sheet.html";
+    if (!game.user.isGM && this.actor.limited) return 'systems/vtm5e/templates/actor/limited-sheet.html'
+    return 'systems/vtm5e/templates/actor/actor-sheet.html'
   }
 
   /* -------------------------------------------- */
