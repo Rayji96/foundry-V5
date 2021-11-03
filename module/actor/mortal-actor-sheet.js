c750bdc429761d822590a17ce27efe372ec4ed33
Ray Ji <rayji96@gmail.com>
2021-11-03 01:14:39 -0700

    set minheight for editor, make tenet-like boxes nicer, hide increasing and decreasing max hp and wp on sheet lock

--- module/actor/mortal-actor-sheet.js
+++ module/actor/mortal-actor-sheet.js
@@ -292,9 +292,9 @@ export class MortalActorSheet extends CoterieActorSheet {
     const element = event.currentTarget
     const dataset = element.dataset
     const resource = dataset.resource
-    if (dataset.action === 'plus') {
+    if (dataset.action === 'plus' && !this.locked) {
       actorData.data[resource].max++
-    } else if (dataset.action === 'minus') {
+    } else if (dataset.action === 'minus' && !this.locked) {
       actorData.data[resource].max = Math.max(actorData.data[resource].max - 1, 0)
     }
 
