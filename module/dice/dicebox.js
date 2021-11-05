eb3ebe0e7933691fda90e242e1f3b20822af87a3
Ray Ji <rayji96@gmail.com>
2021-11-04 17:44:07 -0700

    disabled unique id on htmlhint due to handlebars, fixed a bug in dicebox, fixed a bug with coterie limited sheet

--- module/dice/dicebox.js
+++ module/dice/dicebox.js
@@ -146,14 +146,14 @@ function describePool (poolNumber, data) {
 export function prepareRouseShortcut ($content, data) {
   $content.find('.dice-tray__button[data=rollRouse]').on('click', event => {
     event.preventDefault()
-    rollDice(1, data.character, game.i18n.localize('VTM5E.RousingBlood'), 1, true)
+    rollDice(1, data.selectedCharacter, game.i18n.localize('VTM5E.RousingBlood'), 1, true)
   })
 }
 
 export function prepareWillpowerShortcut ($content, data) {
   $content.find('.dice-tray__button[data=rollWill]').on('click', event => {
     event.preventDefault()
-    const actor = data.character
+    const actor = data.selectedCharacter
     const actorData = actor.data.data
     const dicepool = (actorData.willpower.max - actorData.willpower.aggravated - actorData.willpower.superficial)
     rollDice(dicepool, actor, game.i18n.localize('VTM5E.RollingWillpower'), 0, false)
@@ -163,7 +163,7 @@ export function prepareWillpowerShortcut ($content, data) {
 export function prepareFrenzyShortcut ($content, data) {
   $content.find('.dice-tray__button[data=rollFrenzy]').on('click', event => {
     event.preventDefault()
-    const actor = data.character
+    const actor = data.selectedCharacter
     const actorData = actor.data.data
     const dicepool = (actorData.willpower.max - actorData.willpower.aggravated - actorData.willpower.superficial) + Math.floor(actorData.humanity.value / 3)
     rollDice(dicepool, actor, `${game.i18n.localize('VTM5E.ResistingFrenzy')}...`, 0, false)
@@ -173,7 +173,7 @@ export function prepareFrenzyShortcut ($content, data) {
 export function prepareHumanityShortcut ($content, data) {
   $content.find('.dice-tray__button[data=rollHumanity]').on('click', event => {
     event.preventDefault()
-    const actor = data.character
+    const actor = data.selectedCharacter
     const actorData = actor.data.data
     const dicepool = (10 - actorData.humanity.value - actorData.humanity.stains)
     rollDice(dicepool, actor, game.i18n.localize('VTM5E.RollingRemorse'), 0, false)
