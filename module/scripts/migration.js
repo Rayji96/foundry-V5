b5f6e8625fac965e9184acc924b5fa6e0a243d8a
Veilza <veilzakinsemi@gmail.com>
2024-01-11 04:23:25 -0600

    Adjustments to the Migration function

--- module/scripts/migration.js
+++ module/scripts/migration.js
@@ -22,55 +22,49 @@ export const migrateWorld = async () => {
 
   console.log('Current SchreckNet Layer v' + worldVersion)
 
-  // If the world version is old, then push updates
-  if (worldVersion !== currentVersion || worldVersion === '1.5') {
-    const updates = []
-
-    ui.notifications.info('New version detected; Updating SchreckNet, please wait.')
-    console.log('Obtaining SchreckNet Layer v' + currentVersion)
-
-    // Promise chain to go through the migration functions here
-    // Migrate legacy sheets
-    MigrateLegacySheets()
-      .then(migrationIDs => {
-        // Merge any updates
-        updates.concat(migrationIDs)
-
+  async function updateWorld() {
+    if (worldVersion !== currentVersion || worldVersion === '1.5') {
+      const updates = []
+  
+      ui.notifications.info('New version detected Updating SchreckNet, please wait.')
+      console.log('Obtaining SchreckNet Layer v' + currentVersion)
+  
+      try {
+        // Migrate legacy sheets
+        const migrationIDs1 = await MigrateLegacySheets()
+        updates.concat(migrationIDs1)
+  
         // Migrate localization
-        return MigrateLocalization()
-      })
-      .then(migrationIDs => {
-        // Merge any updates
-        updates.concat(migrationIDs)
-
+        const migrationIDs2 = await MigrateLocalization()
+        updates.concat(migrationIDs2)
+  
         // Migrate gamesystem data
-        return MigrateGamesystem()
-      })
-      .then(migrationIDs => {
-        // Merge any updates
-        updates.concat(migrationIDs)
-
+        const migrationIDs3 = await MigrateGamesystem()
+        updates.concat(migrationIDs3)
+  
         // Migrate health and willpower tracker data
-        return MigrateTrackers()
-      })
-      .then(migrationIDs => {
-        // Merge any updates
-        updates.concat(migrationIDs)
-
+        const migrationIDs4 = await MigrateTrackers()
+        updates.concat(migrationIDs4)
+  
         // Only reload if there's 1 or more updates
         if (updates.length > 0) {
           ui.notifications.info('Upgrade complete! Foundry will now refresh in 10 seconds...')
-
+  
           // Reload to implement the fixes after 10 seconds
-          setTimeout(function () {
+          setTimeout(() => {
             foundry.utils.debouncedReload()
           }, 10000)
         } else {
           ui.notifications.info('No changes necessary! Welcome to version ' + currentVersion)
         }
-      })
-
-    // Update game version
-    game.settings.set('vtm5e', 'worldVersion', currentVersion)
+  
+        // Update game version
+        game.settings.set('vtm5e', 'worldVersion', currentVersion)
+      } catch (error) {
+        console.error('Error during update:', error)
+      }
+    }
   }
+  
+  updateWorld()
 }
