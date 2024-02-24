94aa74f7f7ee9a26a44f060aec92095af8666fde
Veilza <veilzakinsemi@gmail.com>
2024-02-24 08:05:18 -0600

    Add a fix for messy criticals not showing up if the difficulty isn't set

--- module/scripts/rolls/roll-message.js
+++ module/scripts/rolls/roll-message.js
@@ -245,7 +245,12 @@ export async function generateRollMessage ({
           // Handle pluralizing based on the number of successes
           const successText = totalResult > 1 ? 'WOD5E.Roll.Successes' : 'WOD5E.Roll.Success'
 
-          resultLabel = `<div class="roll-result-label">${totalResult} ${game.i18n.localize(successText)}</div>`
+          if (system === 'vampire' && advancedDice.criticals > 1) { // Handle messy criticals if no difficulty is set
+            resultLabel = + `<div class="roll-result-label messy-critical">${game.i18n.localize('WOD5E.VTM.MessyCritical')}</div>
+            <div class="roll-result-label">${totalResult} ${game.i18n.localize(successText)}</div>`
+          } else {
+            resultLabel = `<div class="roll-result-label">${totalResult} ${game.i18n.localize(successText)}</div>`
+          }
         }
       }
     } else {
