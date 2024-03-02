30ae695a8bdf37d8dbacc5f009285a540e9c4c17
Veilza <veilzakinsemi@gmail.com>
2024-03-02 03:28:43 -0600

    Fix an issue where gifts and rites wouldn't show descriptions when clicked, also fixed edge/gift description collapsible styling

--- module/actor/wod-v5-sheet.js
+++ module/actor/wod-v5-sheet.js
@@ -163,21 +163,18 @@ export class WoDActor extends ActorSheet {
       li.slideUp(200, () => this.render(false))
     })
 
-    // Collapsible Features and Powers
-    const coll = document.getElementsByClassName('collapsible')
-    let i
+    // Collapsible items and other elements
+    $('.collapsible').on('click', function () {
+      $(this).toggleClass('active')
+    
+      const content = $(this).closest('.collapsible-container').find('.collapsible-content')
 
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener('click', function () {
-        this.classList.toggle('active')
-        const content = this.parentElement.nextElementSibling
-        if (content.style.maxHeight) {
-          content.style.maxHeight = null
-        } else {
-          content.style.maxHeight = content.scrollHeight + 'px'
-        }
-      })
-    }
+      if (content.css('maxHeight') == '0px') {
+        content.css('maxHeight', content.prop('scrollHeight') + 'px')
+      } else {
+        content.css('maxHeight', '0px')
+      }
+    })
   }
 
   /**
